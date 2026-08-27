cdf8cb151edd8dd839a5690944fa1b74a5e81b72
aniketbumbc <aniket.umbc@gmail.com>
2026-08-27 17:08:49 +0530

    feat: get review with pagination added

--- ticket-review/routes/reviews.py
+++ ticket-review/routes/reviews.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends, Query
 from sqlmodel import Session, select, func
 from models import Review, ReviewCreate, ReviewRead, ReviewUpdate
 from database import get_session
@@ -13,3 +13,20 @@ def create_review(review:ReviewCreate,session:Session = Depends(get_session)):
     session.commit()
     session.refresh(db_review)
     return db_review
+
+
+@router.get("/", response_model=list[ReviewRead])
+def list_reviews(
+    play_name:str | None = Query(None, description="filter by name"),
+    skip:int = Query(0,ge=0,description="Number of reviews to skip"),
+    limit:int = Query(10,ge=1, le=25, description="Max reviews to return"),
+    session:Session = Depends(get_session)
+):
+    query = select(Review)
+
+    if play_name:
+        query = query.where(Review.play_name == play_name)
+
+    query = query.offset(skip).limit(limit)
+    reviews = session.exec(query).all()
+    return reviews
