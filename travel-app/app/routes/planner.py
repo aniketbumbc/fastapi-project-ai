964c88d0e4bb733807caeb57bcf3c82a80049b9e
aniketbumbc <aniket.umbc@gmail.com>
2026-09-03 12:08:15 +0530

    fixed: asyncio implemented for  parallel api call

--- travel-app/app/routes/planner.py
+++ travel-app/app/routes/planner.py
@@ -4,6 +4,10 @@ from app.models.planner import TravelRequest
 from app.services.weather import get_weather_data
 from app.services.location_details import get_location_details
 from app.services.currency import get_currency_rate
+import asyncio
+
+
+
 router = APIRouter(prefix='/api/v1/planner', tags=['planner'])
 
 
@@ -20,16 +24,13 @@ async def create_plan(travel_request: TravelRequest):
     trip_num_days = (travel_request.end_date - travel_request.start_date).days
     if trip_num_days < 1:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Trip duration cannot be less than 1 day')
-        
-    weather_data = get_weather_data(travel_request.destination, travel_request.start_date, travel_request.end_date)
-    location_data = get_location_details(travel_request.destination)
-    currency_data = get_currency_rate(travel_request.currency)
-    # Get the weather data for the destination
-    # Get the location data for the destination
-    # get the currency data for the destination
-    # get the activity data for the destination
-    # aggregate the data to create a travel plan
-    # return the travel plan
+
+
+    weather_data, location_data, currency_data = await asyncio.gather(
+        asyncio.to_thread(get_weather_data, travel_request.destination, travel_request.start_date, travel_request.end_date),
+        asyncio.to_thread(get_location_details, travel_request.destination),
+        asyncio.to_thread(get_currency_rate, travel_request.currency)
+    )
     travel_plan = None
 
     return {"message": "Travel plan created successfully", "weather_data": weather_data, "location_data": location_data, "currency_data": currency_data}
