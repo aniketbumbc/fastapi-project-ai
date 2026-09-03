f593961a8790279f3cc2838623ca9b3dd13e015c
aniketbumbc <aniket.umbc@gmail.com>
2026-09-03 06:16:14 +0530

    feat:  initial setup for travel backend api.

---
+++ travel-app/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI(title='Travel App', description='Aggreate travel data from multiple sources to provide a comprehensive view of travel destinations')
+
+@app.get('/')
+async def root():
+    return {"app": "Travel App",
+    "version": "1.0.0",
+    "enpoints":{
+        "POST /api/v1/plan": "Create a new travel plan (Aggregated)",
+        "GET /api/v1/plan/stream": "Stream a travel plan by Sever send events to client",
+        "GET /api/v1/plan/cache-stash": "Cache stash for travel plan",
+        "DELETE /api/v1/plan/cache-stash": "Delete cache stash for travel plan",
+    }}
